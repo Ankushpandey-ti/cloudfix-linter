aef63bd9fa8a52536687093371a78dc6373f9af6
Ankushpandey-ti <ankush.pandey@trilogy.com>
2023-02-13 19:27:17 +0530

    deleted unnecessary

--- cloudfixIntegration/globalConfigs.go
+++
@@ -1,7 +0,0 @@
-package cloudfixIntegration
-
-const BASE_API_URL string = "https://preview.app.cloudfix.com/api/v1"
-
-const RECOMMENDATIONS_ENDPOINT string = BASE_API_URL + "/ui/recommendations"
-const LOGIN_ENDPOINT string = BASE_API_URL + "/auth/login"
-const ACCOUNTS_ENDPOINT string = BASE_API_URL + "/financial/accounts"
